0411f4764e7f560ace7112a775195ba14d10b72b
Luca Meneghetti <126782219+lucamenee@users.noreply.github.com>
2024-01-10 09:03:17 +0100

    first commit on github

--- src/SoccerTournament.java
+++ src/SoccerTournament.java
@@ -8,13 +8,13 @@ public class SoccerTournament {
     public SoccerTournament(Collection<Match> ms, Collection<String> ts) throws SoccerException {
         this.teams = ts;
         for (Match m : ms) {
-            if (m instanceof SoccerMatch)
+            if (m instanceof SoccerMatch) {
                 this.matches.add(m);
-            else if (!this.teams.contains(m.getTeam1()))
-                throw new SoccerException(m.getTeam1()  + " not in team list for this tournament");
-            else if (!this.teams.contains(m.getTeam2()))
-                throw new SoccerException(m.getTeam2()  + " not in team list for this tournament");
-            else
+                if (!this.teams.contains(m.getTeam1()))
+                    throw new SoccerException(m.getTeam1() + " not in team list for this tournament");
+                else if (!this.teams.contains(m.getTeam2()))
+                    throw new SoccerException(m.getTeam2() + " not in team list for this tournament");
+            } else
                 throw new SoccerException("Match involving " + m.getTeam1() + " and " + m.getTeam2() + " is not a soccer match");
         }
     }
